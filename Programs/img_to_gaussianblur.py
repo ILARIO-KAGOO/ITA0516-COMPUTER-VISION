bce7715219532e58427542dad0f0f9fda563b1c5
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2024-03-01 11:44:04 +0530

    EXP-2

---
+++ Programs/img_to_gaussianblur.py
@@ -0,0 +1,12 @@
+#Perform basic Image Handling and processing operations on the
+#image is Read an image in python and Convert an Image to Blur
+#using Gaussian Blur
+
+import cv2
+path = "D:\Workspace (VS Code)\College\Slot-B-Computer Vision\Input\cat.jpg"
+img =cv2.imread(path)
+cv2.imshow("Original",img)
+# imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(img,(7,7),0)
+cv2.imshow("Gaussian Blur",imgBlur)
+cv2.waitKey(0)
