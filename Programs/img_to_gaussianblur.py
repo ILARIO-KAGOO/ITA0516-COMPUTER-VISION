d3a5eb7aca8017666e08db7c063370befed707f7
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2024-03-01 11:56:59 +0530

    EXP-2

--- Programs/img_to_gaussianblur.py
+++ Programs/img_to_gaussianblur.py
@@ -6,7 +6,7 @@ import cv2
 path = "D:\Workspace (VS Code)\College\Slot-B-Computer Vision\Input\cat.jpg"
 img =cv2.imread(path)
 cv2.imshow("Original",img)
-# imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #for making img B/W
 imgBlur = cv2.GaussianBlur(img,(7,7),0)
 cv2.imshow("Gaussian Blur",imgBlur)
 cv2.waitKey(0)
