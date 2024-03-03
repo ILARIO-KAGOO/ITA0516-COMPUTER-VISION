0c9ae7c8347472872cb9504a8866d686a742623a
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2024-03-03 22:25:31 +0530

    EXP-6

---
+++ Programs/6.fast_slow_video.py
@@ -0,0 +1,16 @@
+#Perform basic video processing operations on the captured video• Read captured video in python and display the video, in slow motion and in fast motion
+
+import cv2
+cap = cv2.VideoCapture("D:\Workspace (VS Code)\College\Slot-B-Computer Vision\Input\car.mp4")
+if (cap.isOpened()== False):
+  print("Error opening video file")
+while(cap.isOpened()):
+  ret, frame = cap.read()
+  if ret == True:
+    cv2.imshow('Frame', frame)
+    if cv2.waitKey(250) & 0xFF == ord('q'):  #waitkey - lesser number (fast) & bigger number (slow)
+      break
+  else:
+    break
+cap.release()
+cv2.destroyAllWindows()
