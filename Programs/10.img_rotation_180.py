7d7fbd16e0e5172a38a3a7a2a407a0b408a71c5e
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2024-03-04 12:40:16 +0530

    EXP-10

---
+++ Programs/10.img_rotation_180.py
@@ -0,0 +1,10 @@
+#ROTATION 180 ALONG DEGREE
+
+import cv2
+path=r"D:\Workspace (VS Code)\College\Slot-B-Computer Vision\Input\cat.jpg"
+src = cv2.imread(path)
+cv2.imshow("Original", src)
+image = cv2.rotate(src, cv2.ROTATE_180)
+cv2.imshow("Rotated", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
