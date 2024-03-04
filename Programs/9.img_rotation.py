069b77114d62324946ab1508f4f35a79ba619362
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2024-03-04 12:32:34 +0530

    EXP-9

--- Programs/9.img_rotation.py
+++ Programs/9.img_rotation.py
@@ -4,7 +4,7 @@ import cv2
 path=r"D:\Workspace (VS Code)\College\Slot-B-Computer Vision\Input\cat.jpg"
 src = cv2.imread(path)
 cv2.imshow("Original", src)
-image = cv2.rotate(src, cv2.ROTATE_180)
+image = cv2.rotate(src, cv2.ROTATE_90_CLOCKWISE)
 cv2.imshow("Rotated", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
