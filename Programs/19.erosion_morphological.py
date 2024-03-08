77237530c5eea288419ae7e725dffdbf04359362
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2024-03-08 09:22:08 +0530

    EXP-19

---
+++ Programs/19.erosion_morphological.py
@@ -0,0 +1,20 @@
+#Implement the Erosion Morphological operations technique
+# using Open CV in python.
+
+import cv2
+import numpy as np
+
+# Read the image
+image = cv2.imread('D:\Workspace (VS Code)\College\Slot-B-Computer Vision\Input\cat.jpg', 0)  # Read the image in grayscale
+
+# Define the kernel for erosion
+kernel = np.ones((5, 5), np.uint8)  # 5x5 kernel with all ones
+
+# Apply erosion
+eroded_image = cv2.erode(image, kernel, iterations=1)
+
+# Display the original and eroded images
+cv2.imshow('Original Image', image)
+cv2.imshow('Eroded Image', eroded_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
